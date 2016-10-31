80699a9a1d7c0bbe3d3b213b6b9675cf9fed6cdd
Karol-Regula <kregula@stuy.edu>
2016-10-31 11:47:41 -0400

    Changed printing statement for size to be easily readble by humans.

--- stat.c
+++ stat.c
@@ -10,9 +10,39 @@ void main(){
     printf("\n");
     
     struct stat info;
-    stat("stat.c", &info);//obtaining information about file
+    stat("a.pdf", &info);//obtaining information about file
     
     printf("Size of file (bytes): %li\n", info.st_size);//printing size of file
+    double out = 0;
+
+
+    char * convert(int scale){
+	scale--;
+	if (scale == 0){
+	    return "bytes";
+	}
+	if (scale == 1){
+	    return "kilobytes";
+	}
+	if (scale == 2){
+	    return "megabytes";
+	}
+	else{
+	    return "failure";
+	}
+    }
+
+    int scale = 0;
+    double compare = 1;
+    while(info.st_size > compare){
+	compare *= 1024;
+	scale ++;
+    }
+    printf("Size of file (%s): %f\n", convert(scale), info.st_size / compare * 1024);
+
+    
+
+
     
     printf("File mode: %o\n", info.st_mode);//printing file permissions
     
