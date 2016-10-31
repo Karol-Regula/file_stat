c3953374b989e137da000a79b3738a200b781815
yawata159 <seijiyawata@gmail.com>
2016-10-31 19:55:53 -0400

    File size prints w/ one decimal place

--- stat.c
+++ stat.c
@@ -26,12 +26,10 @@ char convert(int scale){
 
 void main(){
     printf("\n");
-    
+
     struct stat info;
     stat("stat.c", &info);//obtaining information about file
-    
-    //printf("Size of file (bytes): %li\n", info.st_size);//printing size of file
-    
+
     double out = 0;
     int scale = 0;
     double compare = 1;
@@ -39,18 +37,19 @@ void main(){
 	compare *= 1024;
 	scale ++;
     }
-    printf("Size of file: %f %cB\n", info.st_size / compare * 1024, convert(scale));
+    //printf("Size of file (bytes): %li\n", info.st_size);//printing size of file
+    printf("Size of file: %.1f %cB\n", info.st_size / compare * 1024, convert(scale));
 
 
     char * modes[] = {"---","--x","-w-","-wx","r--","r-x","rw-","rwx"};
     int other = info.st_mode % 8;
     int group = (info.st_mode / 8) % 8;
     int owner = (info.st_mode / 64) % 8;
+    //printf("File mode: %o\n", info.st_mode);
     printf("File mode: %s%s%s\n", modes[owner], modes[group], modes[other]);
-    
 
-    
-    char time[100]; 
+
+    char time[100];
     strftime(time, 100, "%a %b %d %T %Y", localtime(&(info.st_ctime)));//calculating time
     //note: can use gmtime() instead of localtime() to avoid locaion discrepancies
     printf("Time of last access: %s\n", time);//printing time
