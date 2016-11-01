c0893c96ec3c1ecbe209c93b359dcddad36e0f5e
Karol-Regula <kregula@stuy.edu>
2016-11-01 07:21:36 -0400

    Added conversion of file mode to user readable version. Uses ternary operator.

--- stat.c
+++ stat.c
@@ -5,17 +5,18 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <math.h>
 
 void main(){
     printf("\n");
     
     struct stat info;
-    stat("a.pdf", &info);//obtaining information about file
+    stat("b.md", &info);//obtaining information about file
     
     printf("Size of file (bytes): %li\n", info.st_size);//printing size of file
     double out = 0;
 
-
+    //scale conversion
     char * convert(int scale){
 	scale--;
 	if (scale == 0){
@@ -41,10 +42,23 @@ void main(){
     printf("Size of file (%s): %f\n", convert(scale), info.st_size / compare * 1024);
 
     
-
+    //file mode conversion
+    char out2[10] = {0};
+    int n = info.st_mode;
+    out2[0] = '-';
+    out2[1] = (n & S_IRUSR ) ? 'r' : '-';
+    out2[2] = (n & S_IWUSR ) ? 'w' : '-';
+    out2[3] = (n & S_IXUSR ) ? 'x' : '-';
+    out2[4] = (n & S_IRGRP ) ? 'r' : '-';
+    out2[5] = (n & S_IWGRP ) ? 'w' : '-';
+    out2[6] = (n & S_IXGRP ) ? 'x' : '-';
+    out2[7] = (n & S_IROTH ) ? 'r' : '-';
+    out2[8] = (n & S_IWOTH ) ? 'w' : '-';
+    out2[9] = (n & S_IXOTH ) ? 'x' : '-';
 
     
     printf("File mode: %o\n", info.st_mode);//printing file permissions
+    printf("File mode alternate: %s\n", out2);
     
     char time[100]; 
     strftime(time, 100, "%a %b %d %T %Y", localtime(&(info.st_ctime)));//calculating time
@@ -52,4 +66,5 @@ void main(){
     printf("Time of last access: %s\n", time);//printing time
 
     printf("\n");
+    //yawata159
 }
